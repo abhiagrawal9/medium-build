ae973b10d254e8ff953b45378384de49398e0d0a
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-09-26 21:57:47 +0530

    styled post blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
-        <title>Medium Blog</title>
+        <title>Medium Blog</title>s
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
@@ -21,12 +21,29 @@ const Home: NextPage<Props> = ({ posts }) => {
       <Banner />
 
       {/* Posts */}
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
         {posts.map((post: Post) => {
           return (
             <Link key={post._id} href={`/post/${post.slug.current}`}>
-              <div>
-                <img src={urlFor(post.mainImage).url()!} alt={post.title} />
+              <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+                <img
+                  className='w-full h-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
+                  src={urlFor(post.mainImage).url()!}
+                  alt={post.title}
+                />
+                <div className='flex justify-between p-5 bg-white'>
+                  <div>
+                    <p className='text-lg font-bold'>{post.title}</p>
+                    <p className='text-xs'>
+                      {post.description} by {post.author.name}
+                    </p>
+                  </div>
+                  <img
+                    className='h-12 w-12 rounded-full'
+                    src={urlFor(post.author.image).url()}
+                    alt={post.title}
+                  />
+                </div>
               </div>
             </Link>
           );
@@ -40,6 +57,7 @@ export default Home;
 
 import { GetServerSideProps } from 'next';
 import Link from 'next/link';
+import { url } from 'inspector';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const query = `*[_type == 'post']{
