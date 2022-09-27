03ac73dd8946ea8a6f9b96f617639279490d9fdc
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-09-27 08:36:13 +0530

    changes in ui for blog post

--- models/post.model.ts
+++ models/post.model.ts
@@ -23,4 +23,5 @@ export interface Post {
   slug: Slug;
   title: string;
   _createdAt: string;
+  body: any[];
 }
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import { Post } from '../../models/post.model';
 import { sanityClient, urlFor } from '../../sanity';
+import PortableText from 'react-portable-text';
 
 type Props = {
   post: Post;
@@ -32,6 +33,29 @@ const Post = ({ post }: Props) => {
             <span className='text-green-600'>{post.author.name}</span> -
             Published at {new Date(post._createdAt).toLocaleString()}
           </p>
+        </div>
+        <div className='mt-10 '>
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className='text-2xl font-bold my-5' {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className='text-xl font-bold my-5' {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className='ml-4 list-disc'>{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className='text-blue-500 hover:underline'>
+                  {children}
+                </a>
+              ),
+            }}
+          />
         </div>
       </article>
     </main>
