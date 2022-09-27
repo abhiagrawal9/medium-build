325354a52fd8ec1473413cbdc06857e6507192e9
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-09-27 11:04:23 +0530

    populate the comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -152,6 +152,21 @@ const Post = ({ post }: Props) => {
           />
         </form>
       )}
+
+      <div className='flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2'>
+        <h3 className='text-4xl'>Comments</h3>
+        <hr className='pb-2' />
+        {post.comments.map((comment) => {
+          return (
+            <div key={comment._id}>
+              <p>
+                <span className='text-yellow-500'>{comment.name}:</span>{' '}
+                {comment.comment}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 };
